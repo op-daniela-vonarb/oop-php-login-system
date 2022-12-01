6719240ae084a4c3c147e11c485f805677645838
Daniela von Arb <Daniela.vonArb@openpolytechnic.ac.nz>
2022-12-02 11:12:29 +1300

    create signupUser function

--- classes/signup-contr.classes.php
+++ classes/signup-contr.classes.php
@@ -15,6 +15,35 @@ class SignupContr {
         $this->$email = $aemail;
     }
 
+    private function signupUser() {
+        if($this->emptyInput() == false) {
+            // echo "Empty input!";
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+        if($this->invalidUid() == false) {
+            // echo "Invalid username!";
+            header("location: ../index.php?error=username");
+            exit();
+        }
+        if($this->invalidEmail() == false) {
+            // echo "invalid email!";
+            header("location: ../index.php?error=email");
+            exit();
+        }
+        if($this->pwdMatch() == false) {
+            // echo "passwords don't match!";
+            header("location: ../index.php?error=passwordmatch");
+            exit();
+        }
+        if($this->uidTakenCheck() == false) {
+            // echo "username or email taken";
+            header("location: ../index.php?error=useroremailtaken");
+            exit();
+        }
+
+    }
+
     private function emptyInput() {
         $result;
         if(empty($this->$uid) || empty($this->$pwd) || empty($this->$pwdrepeat) || empty($this->$email)) {
@@ -51,6 +80,17 @@ class SignupContr {
     private function pwdMatch() {
         $result;
         if($this->pwd !== $this->pwdrepeat) {
+            $result = false;
+        }
+        else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function uidTakenCheck() {
+        $result;
+        if(!$this->checkUser($this->$uid, $this->$email)) {
             $result = false;
         }
         else {
