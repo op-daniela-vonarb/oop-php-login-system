2d7d900b5e8c4d4d153fac122295ba2692e7f914
Daniela von Arb <Daniela.vonArb@openpolytechnic.ac.nz>
2022-12-02 11:19:46 +1300

    create setUser function

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -3,6 +3,21 @@
 
 class Signup extends Dbh {
 
+    protected function setUser($uid, $pwd, $email) {
+        $stmt = $this->connect()->prepare('INSERT INTO unsers (users_uid, users_pwd, users_email) VALUES (?, ?, ?);');
+
+        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+        if(!$stmt->execute(array($uid, $hashedPwd, $email))) {
+            $stmt = null;
+            header("location: ../index.php?error=stmtfailed");
+            exit();
+        }
+        $stmt = null;
+    
+
+    }
+
     protected function checkUser($uid, $email) {
         $stmt = $this->connect()->prepare('SELECT users_uid FROM users WHERE users_uid = ? OR users_email = ?;');
 
