88397f412b24ab6b87b9b183e5b8dc817a147220
Daniela von Arb <Daniela.vonArb@openpolytechnic.ac.nz>
2022-12-02 09:52:19 +1300

    create signup.inc file

---
+++ includes/signup.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST["submit"]))
+{
+    // Grabbing the data
+    $uid = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+    $email = $_POST["email"];
+
+    // Instantiate SingupContr class
+
+    // Running error handlers and user signup
+
+    // Going to back to front page
+}
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                 <form action="includes/signup.inc.php" method="post">
                     <input type="text" name="uid" placeholder="Username">
                     <input type="password" name="pwd" placeholder="Password">
-                    <input type="password" name="pwd" placeholder="Repeat Password">
+                    <input type="password" name="pwdrepeat" placeholder="Repeat Password">
                     <input type="text" name="email" placeholder="E-Mail">
                     <br>
                     <button type="submit" name="submit">SIGN UP</button>
