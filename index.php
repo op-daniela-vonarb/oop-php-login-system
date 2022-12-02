ea3711a8c5e558ce49b7065c25e9fcaf9a027017
Daniela von Arb <Daniela.vonArb@openpolytechnic.ac.nz>
2022-12-02 14:34:13 +1300

    add nav

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +12,41 @@
     <title>Document</title>
 </head>
 <body>
+
+<header>
+    <nav>
+        <div>
+            <h3>Daniela von Arb</h3>
+            <ul class = "menu-main">
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Member+</a></li>
+            </ul>
+        </div>
+        <ul class="menu-member">
+            <?php
+            if(isset($_SESSION["userid"]))
+            {
+            ?>
+            <li><a href="#"><?php echo $_SESSION["useruid"]; ?></a></li>
+            <li><a href="includes/logout.inc.php" class="header-login-a">LOGOUT</a></li>
+
+            <?php
+            }
+            else
+            {
+
+            ?>
+        
+            <li><a href="#">SIGN UP</a></li>
+            <li><a href="#" class="header-login-a">LOGIN</a></li>
+            <?php
+            }
+            ?>
+        </ul>
+    </nav>
+</header>
+ 
     <section class="index-login">
         <div class="wrapper">
             <div class="index-login-signup">
