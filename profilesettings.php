3fbbbc5fd4f7a2c8724b2bec0e24bb8d6e395b7e
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-01-07 16:38:57 +1300

    create profileinfo.inc

--- profilesettings.php
+++ profilesettings.php
@@ -17,13 +17,15 @@
                 <h3>PROFILE SETTINGS</h3>
                 <p>Change your about section here!</p>
                 <form action="includes/profileinfo.inc.php" method="post">
-                    <textarea name="about" rows="10" cols="30" placeholder="Profile about section..."></textarea>
+                    <textarea name="about" rows="10" cols="30" placeholder="Profile about section..."><?php $profileInfo->fetchAbout($_SESSION["userid"]);?>
+                    </textarea>
                     <br><br>
                     <p>Change your profile page intro here!</p>
                     <br>
-                    <input type="text" name="introtitle" placeholder="Profile title...">
+                    <input type="text" name="introtitle" placeholder="Profile title..." value="<?php $profileInfo->fetchTitle($_SESSION["userid"]);?>">
                     <br><br>
-                    <textarea name="introtext" rows="10" cols="30" placeholder="Profile introduction..."></textarea>
+                    <textarea name="introtext" rows="10" cols="30" placeholder="Profile introduction..."><?php $profileInfo->fetchText($_SESSION["userid"]);?>
+                    </textarea>
                     <br>
                     <button type="submit" name="submit">SAVE</button>
                 </form>
