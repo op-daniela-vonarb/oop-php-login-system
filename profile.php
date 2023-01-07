3bda9ed76d73041f8f1d0ff97a9fd4771623173a
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-01-07 13:51:56 +1300

    create profile page

---
+++ profile.php
@@ -0,0 +1,23 @@
+<?php
+    include_once "partials/header.php"
+
+
+?>
+ 
+    <section class="profile">
+        <div class="profile-bg">
+            <div class="wrapper">
+                <h4>DANIELA</h4>
+            </div>
+            <div class="profile-info-about">
+                <h4>ABOUT</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus tempore quae impedit a id similique ex! Vero, ea ab? Id?</p>
+            </div>
+            <div class="profile-content">
+                <h4>Hi! I'm Daniela</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus tempore quae impedit a id similique ex! Vero, ea ab? Id?</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
