0a13be343a9fc09246f6eec0f14bbdef240b5925
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-01-07 16:03:41 +1300

    update profile page

--- profile.php
+++ profile.php
@@ -14,18 +14,34 @@
         <div class="profile-bg">
             <div class="wrapper">
                 <div class="profile-info">
-                    <h4>DANIELA</h4>
+                    <h4>
+                        <?php
+                            echo $_SESSION["useruid"];
+                        ?>
+                    </h4>
                     <a href="profilesettings.php" class="follow-btn">PROFILE SETTINGS</a>
                 </div>
             </div>
             <div class="profile-info-about">
                 <h4>ABOUT</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus tempore quae impedit a id similique ex! Vero, ea ab? Id?</p>
+                <p>
+                    <?php
+                        $profileInfo->fetchAbout($_SESSION["userid"]);
+                    ?>
+                </p>
             </div>
             <div class="profile-content">
                 <div class="profile-intro">
-                    <h4>Hi! I'm Daniela</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus tempore quae impedit a id similique ex! Vero, ea ab? Id?</p>
+                    <h4>
+                        <?php
+                            $profileInfo->fetchTitle($_SESSION["userid"]);
+                        ?>
+                    </h4>
+                    <p>
+                        <?php
+                            $profileInfo->fetchText($_SESSION["userid"]);
+                        ?>
+                    </p>
                 </div>
             </div>
         </div>
