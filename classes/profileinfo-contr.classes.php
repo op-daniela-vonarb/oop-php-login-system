270183534d99b2b1254a82e49d8b49057214ee8a
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2023-01-07 14:55:50 +1300

    create ProfileInfoContr class

--- classes/profileinfo-contr.classes.php
+++ classes/profileinfo-contr.classes.php
@@ -1,5 +1,43 @@
 <?php
 
 class ProfileInfoContr extends ProfileInfo {
+
+    private $userId;
+    private $userUid;
+
+    public function __construct($userId, $userUid)
+    {
+        $this->userId = $userId;
+        $this->userUid = $userUid;
+    }
+
+    public function defaultProfileInfo() {
+        $profileAbout = 'Tell people about yourself!';
+        $profileTitle = "Hi! I am " . $this->userUid;
+        $profileText = "Welcome to my profile page!";
+        $this->setProfileInfo($profileAbout, $profileTitle, $profileText, $this->userId);
+    }
+
+    public function updateProfileInfo($profileAbout, $profileTitle, $profileText) {
+        // Error handlers
+        if($this->emptyInputCheck($profileAbout, $profileTitle, $profileText) == true) {
+            header("location: ../profilesettings.php?error=emptyinput");
+            exit();
+        }
+
+        // Update profile info
+        $this->setNewProfileInfo($profileAbout, $profileTitle, $profileText, $this->userId);
+    }
+
+    private function emptyInputCheck($profileAbout, $profileTitle, $profileText) {
+        $result = '';
+        if(empty($profileAbout) || empty($profileTitle) || empty($profileText)) {
+            $result = true;
+        }
+        else {
+            $result = false;
+        }
+        return $result;
+    }
     
 }
